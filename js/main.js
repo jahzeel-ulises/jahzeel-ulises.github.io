fb9843fdd62ad4467e5b24791d821be95a6d7879
jahzeel-mendez <mendez.diaz.jahzeel.ulises@gmail.com>
2024-04-09 22:59:01 -0600

    Adds projects and improve page

--- js/main.js
+++ js/main.js
@@ -46,3 +46,41 @@ customElements.define('footer-template',class Footer extends HTMLElement {
             }).catch(e => console.error(e));
     }
 });
+
+customElements.define('project-card',class Card extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        fetch(this.getAttribute("src"))
+            .then(r => r.text())
+            .then(t => {
+                //Parse html
+                let parser = new DOMParser();
+                let html = parser.parseFromString(t, "text/html");
+
+                //Add href to button and image
+                let link = this.getAttribute("link");
+                var image = html.getElementById("image-link");
+                image.setAttribute("href",link);
+
+                //Add image
+                let src = this.getAttribute("image-src");
+                var img = html.getElementById("image-scr");
+                img.src="../"+src; 
+
+                //Add card-title
+                let title = this.getAttribute("card-title");
+                var card_header = html.getElementById("card-title");
+                card_header.textContent = title;
+                
+                //Add paragrah
+                let paragraph = this.getAttribute("paragraph");
+                var p_element = html.getElementById("card-text");
+                p_element.textContent = paragraph;
+
+                this.innerHTML = html.body.innerHTML;
+            }).catch(e => console.error(e));
+
+    }
+});
